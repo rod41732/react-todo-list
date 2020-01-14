9d6518127c46648fdfa3d498301d833754cb547f
Rodchananat <rod8711@gmail.com>
2020-01-14 15:17:29 +0700

    fixed HoC not passing props

--- src/hoc/withTodoApp.js
+++ src/hoc/withTodoApp.js
@@ -2,5 +2,5 @@ import React, {useContext} from 'react'
 import { TodoContext } from "../contexts/todoApp"
 
 export const withTodoApp = (Component) => (...props) => { 
-  return <Component todoApp={useContext(TodoContext)}></Component>
+  return <Component todoApp={useContext(TodoContext)} {...props}></Component>
 }
