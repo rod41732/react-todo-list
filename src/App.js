af296ca6360255278446df2a259824ca32d115c1
Rodchananat <rod8711@gmail.com>
2020-01-16 01:09:08 +0700

    fix import error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import useTodoList from './hooks/useTodoList';
 import { TodoContextProvider } from './contexts/todoApp';
 import TodoApp from "./components/todoApp";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
