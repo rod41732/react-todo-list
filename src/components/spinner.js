75f557dd05bebc2d7a4a78b7556ad89b554f4a2c
Rodchananat <rod8711@gmail.com>
2020-01-18 23:10:30 +0700

    add spinner loading

---
+++ src/components/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loader">Loading...</div>
+    );
+};
+
+
+export default Spinner;
--- src/components/todoMain.js
+++ src/components/todoMain.js
@@ -5,20 +5,30 @@ import TodoHeader from './todoHeader';
 import './todoMain.css';
 import { withTodoApp } from '../hoc/withTodoApp';
 import classnames from 'classnames';
+import Spinner from './spinner';
 
 const TodoMain = ({todoApp}) => {
-  const {state: {expanded: {desktop}}} = todoApp;
-  return <div className={
-    classnames({
-      "todo-main p-4": true,
-      "expand-todo": desktop,
-    })
-    }>
-    <TodoHeader/>
-    <AddTodo/>
-    <TodoList/>
-    {/* <TodoFilter/> */}
-  </div>;
+  const {state: {expanded: {desktop}, init: {finished: ready}}} = todoApp;
+  if (ready) {
+    return <div className={
+      classnames({
+        "todo-main p-4": true,
+        "expand-todo": desktop,
+        "loading": !ready,
+      })
+      }>
+        <TodoHeader/>
+        <AddTodo/>
+        <TodoList/>
+    </div>;
+  }
+  return <div style={{
+    display: "grid",
+    alignItems: "center",
+  }}>
+    <Spinner/>
+  </div>
+  
 }
 
 export default withTodoApp(TodoMain);
